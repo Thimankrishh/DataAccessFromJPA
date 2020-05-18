25d356fd5ca166bca349a81673451c82c1e9934b
ThimanKrishh <mysoftware15@gmail.com>
2020-05-18 21:48:11 +0530

    Add Lombok

--- src/main/java/com/example/demo/EmployeeRepository.java
+++ src/main/java/com/example/demo/EmployeeRepository.java
@@ -9,16 +9,16 @@ public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
     // Using Automatic Custom Queries
     List<Employee> findByName(String name);
-    List<Employee> findByNameAndAgeGreaterThan(String name, int age);
 
+    List<Employee> findByNameAndAgeGreaterThan(String name, int age);
 
 
     // Using Manual Custom Queries
 
-    @Query(value = "SELECT e from Employee e where e.name=name" )
+    @Query(value = "SELECT e from Employee e where e.name='Saman'")
     List<Employee> findByName();
 
-    @Query(value = "SELECT e from Employee e where e.name='Kamal' and e.age>25" )
+    @Query(value = "SELECT e from Employee e where e.name='Kamal' and e.age>25")
     List<Employee> findByNameAndAgeGreaterThan();
 
 }
